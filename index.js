6e5d670965b997a05c442943e14ba5f2cae342be
Kaamibroken <kaamibroken64@gmail.com>
2026-03-16 03:17:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const kk1 = require("./api/kk1");
 const kk2 = require("./api/kk2");
 const hs = require("./api/hs");
 const hs1 = require("./api/hs1");
+const hs2 = require("./api/hs2");
 const vc = require("./api/vc");
 const ivs = require("./api/ivs");  // <-- NEW
 
@@ -49,6 +50,7 @@ app.use("/api/kk1", kk1);
 app.use("/api/kk2", kk2);
 app.use("/api/hs", hs);
 app.use("/api/hs1", hs1);
+app.use("/api/hs2", hs2);
 app.use("/api/vc", vc);
 app.use("/api/ivs", ivs); // <-- NEW
 
